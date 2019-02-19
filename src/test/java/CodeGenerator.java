6c0f7854dfab351e46bff2522b701e2177ddee98
wenziwen <wenzw@zdnst.com>
2019-02-19 11:23:53 +0800

    add needed TODO before first generate

--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -17,7 +17,7 @@ import static com.company.project.core.ProjectConstant.*;
  * 代码生成器，根据数据表名称生成对应的Model、Mapper、Service、Controller简化开发。
  */
 public class CodeGenerator {
-    //JDBC配置，请修改为你项目的实际配置
+    //todo JDBC配置，请修改为你项目的实际配置
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test";
     private static final String JDBC_USERNAME = "root";
     private static final String JDBC_PASSWORD = "123456";
